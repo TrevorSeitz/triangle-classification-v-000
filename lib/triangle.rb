b8c0ff2a8c109deb41491de815a35f7e8ced955e
Trevor Seitz <trevinla@gmail.com>
2018-03-26 19:15:46 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -22,9 +22,9 @@ class Triangle
     end
 
     if @side1 == @side2 && @side1 == @side3
-      binding.pry
       return :equilateral
     elsif @side1 != @side2 && @side1 != @side3 && @side2 != @side3
+      binding.pry
       return :scalene
     else
       return :isosceles
